5d0f496f5d0b6da685e94f35279e341f7cb3c5a1
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-27 10:15:09 +0200

    styles for FormButton added

---
+++ src/features/SingIn/Form/FormButton/styled.js
@@ -0,0 +1,19 @@
+export const StyledButton = {
+    mt: 3,
+    mb: 2,
+    bgcolor: "primary.electricBlue",
+    height: "56px",
+    borderRadius: "36px",
+    "&:hover, &:focus": {
+      bgcolor: "primary.electricBlue",
+      fontWeight: "typography.fontWeightMedium",
+    }
+  }
+
+  export const ButtonText = {
+    color: "common.white",
+    textTransform: "none",
+    fontWeight: "typography.fontWeightMedium",
+    fontSize: "fontSize.medium",
+    lineHeight: "lineHeight.small",
+  }
