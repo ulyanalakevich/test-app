05b1a4c0a63862bae42f7b4191e8e4cbc0ec2f5e
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-27 10:17:39 +0200

    socials data added

---
+++ src/features/SingIn/SocialMediaList/socials.js
@@ -0,0 +1,26 @@
+import {ReactComponent as GoogleIcon} from "./icons/google.svg"
+import {ReactComponent as AppleIcon} from "./icons/apple.svg"
+import {ReactComponent as TwitterIcon} from "./icons/twitter.svg"
+import {ReactComponent as FacebookIcon} from "./icons/facebook.svg"
+export const socials = [
+    {
+        name:"Google",
+        url: "https://google.com/",
+        IconComponent: GoogleIcon
+    },
+    {
+        name:"Apple",
+        url: "https://www.apple.com/",
+        IconComponent: AppleIcon
+    },
+    {
+        name:"Twitter",
+        url: "https://twitter.com/",
+        IconComponent: TwitterIcon
+    },
+    {
+        name:"Facebook",
+        url: "https://facebook.com/",
+        IconComponent: FacebookIcon
+    },
+]
