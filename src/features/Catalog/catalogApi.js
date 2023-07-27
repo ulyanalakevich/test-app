ad2c2896e6a1c2752d6073a07ff0eb928a49313c
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-27 18:22:22 +0200

    catalog Api url changed to https

--- src/features/Catalog/catalogApi.js
+++ src/features/Catalog/catalogApi.js
@@ -1,7 +1,7 @@
 export async function getUniversities() {
   try {
     const response = await fetch(
-      "http://universities.hipolabs.com/search?country=United+States"
+      "https://universities.hipolabs.com/search?country=United+States"
     );
     if (!response.ok) {
       throw new Error("Failed to fetch data from the API.");
