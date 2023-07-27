83f7f1d7a2e1e236816212a67af3fbb207384a8d
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-27 10:16:35 +0200

    Switch for RememberMe created

---
+++ src/features/SingIn/Form/RememberMe/SwitchComponent/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+const SwitchComponent = () => {
+  return (
+    <label className="switch">
+      <input type="checkbox" />
+      <span className="slider" />
+    </label>
+  );
+};
+
+export default SwitchComponent;
