486399500cd322e1c2bc217adf37208b7a4974e5
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-27 17:09:45 +0200

    catalog Saga created

---
+++ src/features/Catalog/catalogSaga.js
@@ -0,0 +1,16 @@
+import { put, takeLatest, call } from "redux-saga/effects";
+import { fetchUniversitiesSuccess, fetchUniversitiesError, fetchUniversities } from "./catalogSlice";
+import { getUniversities } from "./catalogApi";
+
+function* fetchUniversitiesHandler() {
+  try {
+    const universities = yield call(getUniversities);
+    yield put(fetchUniversitiesSuccess(universities.slice(0, 100))); 
+  } catch (error) {
+    yield put(fetchUniversitiesError());
+  }
+}
+
+export function* catalogSaga() {
+  yield takeLatest(fetchUniversities.type, fetchUniversitiesHandler);
+}
