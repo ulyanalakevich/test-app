f0ee62063ff3677b0610e110744d6798cd797d74
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-27 10:17:59 +0200

    styles for socials added

---
+++ src/features/SingIn/SocialMediaList/styled.js
@@ -0,0 +1,20 @@
+export const iconStyle = {
+    width: "48px",
+    height: "48px",
+    borderRadius: "40px",
+    backgroundColor: "primary.midnightBlue700",
+    paddingBottom: "2px",
+    alignItems: "center",
+    justifyContent: "center",
+    "&:hover": {
+      backgroundColor: "primary.midnightBlue600",
+    },
+  };
+
+ export const SocialList = {
+    display: "grid",
+    gridTemplateColumns: "repeat(4, 48px)", 
+    gap: "16px",
+    justifyContent: "center",
+    paddingTop: "20px",
+  };
