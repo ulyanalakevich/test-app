d0a4acb01d000db2f89b216d34fd6b556e1de97c
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-27 17:07:44 +0200

    api request to auth added

---
+++ src/features/SingIn/authApi.js
@@ -0,0 +1,24 @@
+export const loginAPI = async (username, password) => {
+    try {
+      const response = await fetch("https://playground.tesonet.lt/v1/tokens", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username: "tesonet",
+            password: "partyanimal",
+        }),
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+  
+      const data = await response.json();
+      return data.token;
+    } catch (error) {
+      throw error.message || "An error occurred during login.";
+    }
+  };
