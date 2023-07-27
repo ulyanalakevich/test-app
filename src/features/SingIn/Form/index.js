462ef0a2ed3b0c6177d96b1c89ec47857f014b9c
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-27 17:12:27 +0200

    handleSubmit function deleted from Form component

--- src/features/SingIn/Form/index.js
+++ src/features/SingIn/Form/index.js
@@ -1,24 +1,10 @@
 import { Box, Grid } from "@mui/material";
 import Inputs from "./Inputs";
-import FormButton from "./FormButton";
 import RememberMe from "./RememberMe";
 
 const Form = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
   return (
-    <Box
-      component="form"
-      color="common.white"
-      noValidate
-      onSubmit={handleSubmit}
-    >
+    <Box>
       <Grid>
         <Inputs />
       </Grid>
