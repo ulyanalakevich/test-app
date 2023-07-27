9fa896ecdf91e051dfdef2259026b79b31d4f381
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-27 10:12:15 +0200

    ThemeProvider applied

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+import Inputs from "./features/SingIn/Form/Inputs";
+import RememberMe from "./features/SingIn/Form/RememberMe";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
