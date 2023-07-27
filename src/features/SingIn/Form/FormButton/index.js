720dbeb57dab3154253a525094c9456d09fadf9d
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-27 17:10:57 +0200

    formatted code

--- src/features/SingIn/Form/FormButton/index.js
+++ src/features/SingIn/Form/FormButton/index.js
@@ -1,24 +1,20 @@
 import { Button, Typography } from "@mui/material";
 import { ButtonText, StyledButton } from "./styled";
 
-const FormButton = ({handleLogin}) => {
-
-return (
-  <Button
+const FormButton = ({ handleLogin }) => {
+  return (
+    <Button
       type="button"
       fullWidth
       variant="contained"
       sx={StyledButton}
       onClick={handleLogin}
     >
-      <Typography
-        variant="body1"
-        sx={ButtonText}
-      >
+      <Typography variant="body1" sx={ButtonText}>
         Sign In
       </Typography>
     </Button>
-)
-}
+  );
+};
 
 export default FormButton;
