592d96a2256b51bdcd167c4c258e6dd2e6176ea2
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-07-31 19:10:10 +0200

    Route Catalog modified, isAuthenticated dependency

--- src/core/App.js
+++ src/core/App.js
@@ -1,17 +1,27 @@
-import React from "react";
 import SingIn from "../features/SingIn";
 import Catalog from "../features/Catalog";
-import { HashRouter, Route, Switch, Routes } from "react-router-dom";
-import { Provider } from "react-redux";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Provider, useSelector } from "react-redux";
 import store from "./store";
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.token !== null);
+
   return (
     <Provider store={store}>
       <HashRouter>
         <Switch>
-          <Route exact path="/" component={SingIn} />
-          <Route path="/catalog" component={Catalog} />
+          <Route exact path="/login" component={SingIn} />
+          <Route
+            exact
+            path="/catalog"
+            render={() => {
+              return isAuthenticated ? <Catalog /> : <Redirect to="/login" />;
+            }}
+          />
+          <Route path="/">
+            <Redirect to="/login" />
+          </Route>
         </Switch>
       </HashRouter>
     </Provider>
